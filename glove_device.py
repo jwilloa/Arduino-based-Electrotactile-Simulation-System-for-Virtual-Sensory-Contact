3a2bd3c6fcbc2c3c7a13a830c8379c9a39e5f3a9
Willock, Javan <jwilloa@essex.ac.uk>
2019-11-05 10:43:06 +0000

    Adding device to Motionbuilder scene with Python script

---
+++ glove_device.py
@@ -0,0 +1,11 @@
+from pyfbsdk import *
+import os
+
+# Access system properties
+scene = FBSystem().Scene
+
+# Create a 5DT Glove device and append it to the scene.
+gloveDevice = FBCreateObject("Browsing/Templates/Devices", "5DT DataGlove", "5DT DataGlove")
+gloveDevice.OnLine = True
+gloveDevice.Live = True
+scene.Devices.append(gloveDevice)
