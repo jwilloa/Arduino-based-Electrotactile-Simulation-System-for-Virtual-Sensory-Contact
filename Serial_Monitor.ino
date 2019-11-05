349b648c4103e27754a4c675fde078f49d036d95
Willock, Javan <jwilloa@essex.ac.uk>
2019-11-05 10:33:21 +0000

    Arduino code to monitor serial channel for incoming data

---
+++ Serial_Monitor.ino
@@ -0,0 +1,38 @@
+/*
+  Serial Monitor
+
+  Arduino boards turns on an LED when it receives the character
+  'H', and turns off the LED when it receives the character 'L'.
+
+  The data is received to the Arduino Serial Monitor, via a Python script.
+
+  The circuit:
+  - LED connected from digital pin 13 to ground
+
+*/
+
+const int ledPin = 13; // the pin that the LED is attached to
+int incomingByte;      // a variable to read incoming serial data into
+
+void setup() {
+  // initialize serial communication:
+  Serial.begin(9600);
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // see if there's incoming serial data:
+  if (Serial.available() > 0) {
+    // read the oldest byte in the serial buffer:
+    incomingByte = Serial.read();
+    // if it's a capital H (ASCII 72), turn on the LED:
+    if (incomingByte == 'H') {
+      digitalWrite(ledPin, HIGH);
+    }
+    // if it's an L (ASCII 76) turn off the LED:
+    if (incomingByte == 'L') {
+      digitalWrite(ledPin, LOW);
+    }
+  }
+}
