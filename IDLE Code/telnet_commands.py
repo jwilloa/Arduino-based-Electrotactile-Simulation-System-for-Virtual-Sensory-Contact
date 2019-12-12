b5a9f1fbfb9550b0aa89fe0994cacf90642a968b
jwilloa <Kindl3Unlimit3d!>
2019-12-12 14:33:08 +0000

    Real-time data of model cube vector

---
+++ IDLE Code/telnet_commands.py
@@ -0,0 +1,23 @@
+import telnetlib
+tn = telnetlib.Telnet("127.0.0.1", 4242)
+
+#Needed to get to the point in the PythonShell where you can write code.
+while True:
+    tn.read_until('>>>', 0.1)
+    tn.write( "myCube2 = FBFindModelByLabelName('Cube')\n" )
+
+    tn.read_until('>>>', 0.11)
+    tn.write( "cube2 = FBVector3d()\n" )
+
+    tn.read_until('>>>', 0.11)
+    tn.write( "print(myCube2.GetVector(cube2))\n" )
+
+    tn.read_until('>>>', 0.11)
+    tn.write( "print cube2\n" )
+
+    raw = str(tn.read_until('>>>', .1))
+
+    print raw
+
+
+tn.close()
