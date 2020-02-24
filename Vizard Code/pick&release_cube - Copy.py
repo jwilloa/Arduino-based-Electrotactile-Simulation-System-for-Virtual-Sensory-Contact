84acb76faffd383fe1ed1d67de584092cfa7a6e1
jwilloa <Kindl3Unlimit3d!>
2020-02-24 16:58:36 +0000

    Working on integrating the raw data values to control the grabbing of the ball. At the moment it's not working as intended and I need to find a way to successfully allow for movement of ball once the grab has been made

--- Vizard Code/pick&release_cube - Copy.py
+++ Vizard Code/pick&release_cube - Copy.py
@@ -3,14 +3,17 @@ import viz
 import vizact
 import time
 import viztracker
+from gloveFunctions import FiveDTGlove
 
 viz.setMultiSample(4)
 viz.fov(60)
 viz.go()# Start an empty world
+glove = FiveDTGlove()
+glove.open("USB0")
 
-
-gallery = viz.addChild( 'ground.osgb')								# Add a model of a gallery into the virtual world
+gallery = viz.addChild( 'ground.osgb' )								# Add a model of a gallery into the virtual world
 viz.collision(viz.ON)												# Stop navigation through 3D models
+
 
 hand = viz.add('white_ball.wrl')									# Add the object that will do the grabbing
 
@@ -36,11 +39,11 @@ link = None 														# The handle to the link object
 													
 def grabBall():
 	object = viz.pick() 											# Command detects which object the mouse is currently over and returns it
-	
 	if object == basketball:										# Check to see if object is basketball
 		print "You've clicked on the basketball"
 		global link
 		link = viz.grab( hand, basketball ) 						# Use hand to grab basketball
+		glove.getSensorRawAll()
 	else:
 		print "This is not a basketball"
 		
@@ -49,6 +52,11 @@ def releaseBall():
 	link.remove()
 	print "You have released the ball"
 	link = None
-		
-vizact.onmousedown(viz.MOUSEBUTTON_LEFT,grabBall)					# When left mouse button is clicked, go to pickBall function
+
+
+if glove.getSensorRawAll > 2300:
+	print "hi there"
+	grabBall
+
+#vizact.onmousedown(viz.MOUSEBUTTON_LEFT,grabBall)					# When left mouse button is clicked, go to pickBall function
 vizact.onmouseup(viz.MOUSEBUTTON_LEFT,releaseBall)
