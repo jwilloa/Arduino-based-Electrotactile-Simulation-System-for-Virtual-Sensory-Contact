76cfaaf7d273318689bc70a79289629355771ce0
jwilloa <Kindl3Unlimit3d!>
2020-02-27 12:08:43 +0000

    Got the 5DT hand grip working somewhat when mouse if hovering over the target object.

--- Vizard Code/Basic_Hand_Test.py
+++ Vizard Code/Basic_Hand_Test.py
@@ -1,44 +1,126 @@
+####### IMPORT FUNCTIONS ###########
 import viz
 import vizact
 import hand
+import time
+import viztracker
 
+#####################################
 
+####### SET WORLD VALUES ###########
 viz.setMultiSample(4)
 viz.fov(60)
 viz.go()
+####################################
 
-#Identify the data glove's port.
-PORT_5DT_USB = 0
+########## SET PORTS ###############
+PORT_5DT_USB = 0  # Identify the data glove's port.
+sensor = viz.addSensor('5dt.dls')  # Add the 5DT sensor
+glove = hand.add(sensor, hand.GLOVE_5DT)  # Create a hand object from the data glove
+####################################
 
-#Add the 5DT sensor
-sensor = viz.addSensor('5dt.dls')
-
-#Create a hand object from the data glove
-glove = hand.add(sensor,hand.GLOVE_5DT)
-
-#Place the hand in front of the user
-glove.setEuler([0,-90,0])
-viz.MainView.setPosition([0,0.02,-0.4])
-
-
-#Add an array with all the gesture names from the 5DT user's manual.
+# Add an array with all the gesture names from the 5DT user's manual.
 gestureName = ['Fist', 'Index finger point', 'Middle finger point',
-'Two finger point', 'Ring finger point', 'Ring-Index finger point',
-'Ring-middle finger point', 'Three finger point', 'Little finger point',
-'Index and little finger point', 'Little-middle finger point',
-'Not ring finger point', 'Little-ring finger point',
-'Not middle finger point', 'Not index finger point',
-'Flat hand', 'Undefined']
+               'Two finger point', 'Ring finger point', 'Ring-Index finger point',
+               'Ring-middle finger point', 'Three finger point', 'Little finger point',
+               'Index and little finger point', 'Little-middle finger point',
+               'Not ring finger point', 'Little-ring finger point',
+               'Not middle finger point', 'Not index finger point',
+               'Flat hand', 'Undefined']
 
-gestureText = viz.addText( '', viz.SCREEN )
-gestureText.setPosition(0.5,0.1)
+gestureText = viz.addText('', viz.SCREEN)
+gestureText.setPosition(0.5, 0.1)
 gestureText.alignment(viz.ALIGN_CENTER)
 
-def getGesture():
+########## ADDITIONAL STUFF ###############
+hand = viz.add('white_ball.wrl')  # Add the object that will do the grabbing
+
+# Link the hand to a 3D mousetracker
+mouseTracker = viztracker.MouseTracker()
+mouseTracker.scroll(-8)
+viz.link(mouseTracker, hand)
+
+viz.mouse(viz.OFF)  # Disable mouse navigation
+
+# Link the hand to a 3D mousetracker
+mouseTracker = viztracker.MouseTracker()
+mouseTracker.scroll(-8)
+viz.link(mouseTracker, hand)
+
+viz.mouse(viz.OFF)  # Disable mouse navigation
+
+# Add a box model and set its position
+basketball = viz.addChild('basketball.osgb')
+basketball.setPosition([0, 2, 1.5])
+
+link = None
+
+
+def grabBall():
     gesture = int(sensor.get()[-1])
     gestureText.message(gestureName[gesture])
-    if gesture == 0:
-        print "closed fist"
-    if gesture == 15:
-        print "open hand"
-vizact.ontimer(0, getGesture)
+    object = viz.pick()  # Command detects which object the mouse is currently over and returns it
+
+    if (gesture == 0) and (object == basketball):  # Check to see if object is basketball
+        print
+        "closed fist - gripped basketball"
+        global link
+        link = viz.grab(hand, basketball)  # Use hand to grab basketball
+        glove.getSensorRawAll()
+
+    else:
+        print
+        "This is not a basketball"
+
+        vizact.ontimer(1, releaseBall)
+        vizact.ontimer(2, releaseBall)
+        vizact.ontimer(3, releaseBall)
+        vizact.ontimer(4, releaseBall)
+        vizact.ontimer(5, releaseBall)
+        vizact.ontimer(6, releaseBall)
+        vizact.ontimer(7, releaseBall)
+        vizact.ontimer(8, releaseBall)
+        vizact.ontimer(9, releaseBall)
+        vizact.ontimer(10, releaseBall)
+        vizact.ontimer(11, releaseBall)
+        vizact.ontimer(12, releaseBall)
+        vizact.ontimer(13, releaseBall)
+        vizact.ontimer(14, releaseBall)
+        vizact.ontimer(15, releaseBall)
+        vizact.ontimer(16, releaseBall)
+
+
+def releaseBall():
+    global link
+    link.remove()
+    print
+    "open hand - released grip on basketball"
+    link = None
+
+
+# gesture = int(sensor.get()[-1])
+# print gesture
+#
+# notgesture = not(int(sensor.get()[-1]))
+# print "result", notgesture
+
+# vizact.ontimer(gesture, grabBall)
+# vizact.ontimer(notgesture, releaseBall)
+
+vizact.ontimer(0, grabBall)
+vizact.ontimer(15, releaseBall)
+# vizact.ontimer(2, releaseBall)
+# vizact.ontimer(3, releaseBall)
+# vizact.ontimer(4, releaseBall)
+# vizact.ontimer(5, releaseBall)
+# vizact.ontimer(6, releaseBall)
+# vizact.ontimer(7, releaseBall)
+# vizact.ontimer(8, releaseBall)
+# vizact.ontimer(9, releaseBall)
+# vizact.ontimer(10, releaseBall)
+# vizact.ontimer(11, releaseBall)
+# vizact.ontimer(12, releaseBall)
+# vizact.ontimer(13, releaseBall)
+# vizact.ontimer(14, releaseBall)
+# vizact.ontimer(15, releaseBall)
+# vizact.ontimer(16, releaseBall)
